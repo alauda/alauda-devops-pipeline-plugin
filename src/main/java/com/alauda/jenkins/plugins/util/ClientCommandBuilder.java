ba51dba13693f19c717d677ff334e2b49ac91c13
cizezsy <zsy19980307@gmail.com>
2018-12-06 18:03:06 +0800

    [DEVOPS-892] Fix 'withCluster' issue

--- src/main/java/com/alauda/jenkins/plugins/util/ClientCommandBuilder.java
+++ src/main/java/com/alauda/jenkins/plugins/util/ClientCommandBuilder.java
@@ -75,19 +75,19 @@ public class ClientCommandBuilder implements Serializable {
         String toolName = (new Devops.DescriptorImpl()).getClientToolName();
         cmd.add(toolName);
 
-//        if (this.server != null) {
-//            cmd.add("--server=" + server);
-//        }
-//
-//        cmd.addAll(toStringArray(this.advArgs));
-//
-//        if (this.skipTLSVerify) {
-//            cmd.add("--insecure-skip-tls-verify");
-//        } else {
-//            if (this.caPath != null) {
-//                cmd.add("--certificate-authority=" + this.caPath);
-//            }
-//        }
+        if (this.server != null) {
+            cmd.add("--server=" + server);
+        }
+
+        cmd.addAll(toStringArray(this.advArgs));
+
+        if (this.skipTLSVerify) {
+            cmd.add("--insecure-skip-tls-verify");
+        } else {
+            if (this.caPath != null) {
+                cmd.add("--certificate-authority=" + this.caPath);
+            }
+        }
 
         if (this.project != null) {
             if (!hasArg(cmd, "-n", "--namespace")) { // only set namespace if
@@ -97,25 +97,25 @@ public class ClientCommandBuilder implements Serializable {
             }
         }
 
-//        // Some arguments may be long and provide little value (e.g. the path of
-//        // the server CA),
-//        // so hide them unless we are in logLevel mode.
-//        if (!redacted || logLevel > 0) {
-//            if (!hasArg(cmd, "--loglevel")) {
-//                cmd.add("--loglevel=" + logLevel);
-//            }
-//        }
-//
-//        String token = this.token;
-//        if (redacted && token != null) {
-//            token = "XXXXX";
-//        }
-//
-//        if (token != null) {
-//            if (!hasArg(cmd, "--token")) { // only set if not specified
-//                cmd.add("--token=" + token);
-//            }
-//        }
+        // Some arguments may be long and provide little value (e.g. the path of
+        // the server CA),
+        // so hide them unless we are in logLevel mode.
+        if (!redacted || logLevel > 0) {
+            if (!hasArg(cmd, "-v")) {
+                cmd.add("-v=" + logLevel);
+            }
+        }
+
+        String token = this.token;
+        if (redacted && token != null) {
+            token = "XXXXX";
+        }
+
+        if (token != null) {
+            if (!hasArg(cmd, "--token")) { // only set if not specified
+                cmd.add("--token=" + token);
+            }
+        }
 
         cmd.add(verb);
 
