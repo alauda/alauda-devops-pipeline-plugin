a711c40430f39d7cce69c9ffbd7a6e09252952b5
Zhao Xiaojie <LinuxSuRen@gmail.com>
2020-07-21 15:37:04 +0800

    Fixes issues found by sonarqube

--- src/main/java/com/alauda/jenkins/plugins/ClusterConfig.java
+++ src/main/java/com/alauda/jenkins/plugins/ClusterConfig.java
@@ -192,7 +192,7 @@ public class ClusterConfig extends AbstractDescribableImpl<ClusterConfig> implem
                                               @QueryParameter String credentialsId,
                                               @QueryParameter String serverCertificateAuthority,
                                               @QueryParameter boolean skipTlsVerify) {
-            LOGGER.log(Level.INFO, "verify connection to {}, skip tls {}", new Object[]{serverUrl, skipTlsVerify});
+            LOGGER.log(Level.INFO, "verify connection to {0}, skip tls {1}", new Object[]{serverUrl, skipTlsVerify});
             String token;
             try {
                 token = CredentialsUtils.getToken(credentialsId);
